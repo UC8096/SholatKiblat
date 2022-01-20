46664bb8495160c544ef011e32ef0b1e34fb4eb8
salis2 <56523603+salis2@users.noreply.github.com>
2022-01-21 00:14:28 +0700

    add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sholatkiblat/helpers/helpers.dart';
 import 'package:sholatkiblat/routes/routes.dart';
+import 'controllers/controllers.dart';
+import 'package:dcdg/dcdg.dart';
 
-void main() {
+int? initScreen;
+
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final OnBoardingController onBoardingController =
+      Get.put(OnBoardingController());
+
+  initScreen = await onBoardingController.initFirstScreen();
+
+  NotificationHelper.initialize();
+
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
 
   runApp(const MyApp());
 }
@@ -14,7 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      initialRoute: '/onboardingpage',
+      initialRoute: initScreen == null ? '/onboarding' : '/home',
       getPages: AppRoute.getPages(),
     );
   }
