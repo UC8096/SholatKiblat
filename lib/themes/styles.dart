4341e9e48f66bfeba2e3e57b76790c0d1ef0b420
salis2 <56523603+salis2@users.noreply.github.com>
2022-01-21 00:08:55 +0700

    add styles

--- lib/themes/styles.dart
+++ lib/themes/styles.dart
@@ -1,9 +1,15 @@
 part of 'themes.dart';
 
 Color blueColor = const Color(0xff579AFD);
+Color whiteColor = const Color(0xffF8F8FA);
 Color grayColor = const Color(0xffEAEBF0);
+Color littledarkgrayColor = const Color(0xffD7D8DC);
 Color darkgrayColor = const Color(0xff848484);
+Color subuhColor = const Color(0xff06B192);
+Color zuhurColor = const Color(0xffE7DC79);
 Color asharColor = const Color(0xffFFA800);
+Color magribColor = const Color(0xff0693B1);
+Color isyaColor = const Color(0xff0669B1);
 
 TextStyle poppins = GoogleFonts.poppins();
 TextStyle blackBoldPoppins =
@@ -12,3 +18,5 @@ TextStyle whiteBoldPoppins =
     poppins.copyWith(color: Colors.white, fontWeight: FontWeight.w800);
 TextStyle darkgreyBoldPoppins =
     poppins.copyWith(color: darkgrayColor, fontWeight: FontWeight.w800);
+TextStyle whiteyBoldPoppins =
+    poppins.copyWith(color: whiteColor, fontWeight: FontWeight.w800);
