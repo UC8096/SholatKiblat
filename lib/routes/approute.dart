e19fb94d5fe640224e6427e85ae2526faa50f656
salis2 <56523603+salis2@users.noreply.github.com>
2022-01-21 00:09:23 +0700

    add route app

--- lib/routes/approute.dart
+++ lib/routes/approute.dart
@@ -2,11 +2,16 @@ part of 'routes.dart';
 
 class AppRoute {
   static List<GetPage> getPages() => [
-        GetPage(name: "/onboardingpage", page: () => const OnBoardingPage()),
-        GetPage(name: "/jadwalpage", page: () => const JadwalPage()),
-        GetPage(name: "/kiblatpage", page: () => KiblatPage()),
+        GetPage(name: "/onboarding", page: () => const OnBoardingPage()),
+        GetPage(name: "/jadwal", page: () => const JadwalPage()),
+        GetPage(name: "/kiblat", page: () => const KiblatPage()),
+        GetPage(name: "/konten", page: () => const KontenPage()),
+        GetPage(name: "/detail", page: () => const DetailWidget()),
         GetPage(
-            name: "/homepage",
+            name: "/createOrUpdate",
+            page: () => const CreateOrUpdateKontenWidget()),
+        GetPage(
+            name: "/home",
             page: () => const HomePage(),
             binding: HomeBinding()),
       ];
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:sholatkiblat/bindings/bindings.dart';
 import 'package:sholatkiblat/pages/pages.dart';
+import 'package:sholatkiblat/pages/widgets/widgets.dart';
 
 part 'approute.dart';
